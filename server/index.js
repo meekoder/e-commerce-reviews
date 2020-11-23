2593c81e20d79a088cbde48ce0a9a3b13c0c54c9
Andrew Kwong <andy.kwong@me.com>
2020-11-22 18:06:46 -0800

    update mongoose connection for docker

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const parser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 const reviewRouter = require('./reviewRouter.js');
-mongoose.connect('mongodb://localhost:27017/reviews');
-const app = express();
 
+const app = express();
 const port = 3004;
 
-app.use(express.static(__dirname + '/../public'));
-app.use(parser.json());
-app.use('/api/products', reviewRouter);
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+mongoose.connect('mongodb://database/reviews',
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.use(express.static(`${__dirname}/../public`));
+    app.use(parser.json());
+    app.use('/api/products', reviewRouter);
+    app.get('/:itemId', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+  });
